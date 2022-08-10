792156779734e281a5d6ac8826a998d9dbfe5875
Sebastian Ohl <sebastian@ohl.de>
2022-08-10 14:31:40 +0200

    adding streaming server

--- main/httpd.c
+++ main/httpd.c
@@ -31,6 +31,102 @@ static size_t jpg_encode_stream(void *arg, size_t index, const void *data,
     return len;
 }
 
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE =
+    "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART =
+    "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t *_jpg_buf;
+    char *part_buf[64];
+    static int64_t last_frame = 0;
+    if (!last_frame)
+    {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if (res != ESP_OK)
+    {
+        return res;
+    }
+
+    while (true)
+    {
+        gpio_set_level(FLASH_GPIO, 1);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+
+        fb = esp_camera_fb_get();
+        if (!fb)
+        {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+            ESP_LOGI(TAG, "encode JPEG");
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+
+            if (!jpeg_converted)
+            {
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        }
+        else
+        {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY,
+                                        strlen(_STREAM_BOUNDARY));
+        }
+        if (res == ESP_OK)
+        {
+            size_t hlen =
+                snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf,
+                                        _jpg_buf_len);
+        }
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        gpio_set_level(FLASH_GPIO, 0);
+        if (res != ESP_OK)
+        {
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+                 (uint32_t)(_jpg_buf_len / 1024), (uint32_t)frame_time,
+                 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
+
 esp_err_t jpg_httpd_handler(httpd_req_t *req)
 {
     camera_fb_t *fb = NULL;
@@ -88,6 +184,11 @@ static const httpd_uri_t uri_root_handler = {.uri = "/",
                                              .method = HTTP_GET,
                                              .handler = jpg_httpd_handler,
                                              .user_ctx = NULL};
+static const httpd_uri_t uri_stream_handler = {.uri = "/stream",
+                                               .method = HTTP_GET,
+                                               .handler =
+                                                   jpg_stream_httpd_handler,
+                                               .user_ctx = NULL};
 
 httpd_handle_t driveway_httpd_start_webserver(void)
 {
@@ -105,6 +206,7 @@ httpd_handle_t driveway_httpd_start_webserver(void)
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &uri_root_handler);
+        httpd_register_uri_handler(server, &uri_stream_handler);
         return server;
     }
 
